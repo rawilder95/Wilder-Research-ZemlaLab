8a11c02861c31361b73d4ad4b5df041fbdfd8e61
REBECCA Anne WILDER <71036276+rawilder95@users.noreply.github.com>
2021-10-05 23:38:03 -0400

    Clean Up
    Last thing before taking a break from the data.  This isn't specific to any comparison, but might make things easier in the future.  Basically I just added a col for (... sp's/rp's?) in trial 2 that correspond to the order that items were listed in trial 1.
    
    E.g. Trial 1: ["A", "B", "C", "D"]
    ["A"= 1, "B"= 2, "C"= 3, "D"=4] --> [1, 2, 3, 4]
    
    Trial 2: ["C", "A", "B", "D"]
    [C= 3, A= 1, B= 2, D= 4] --> [3, 1, 2, 4]
    
    Might not need it, but didn't take a long time and could be useful.

--- fixingsfcode1022021.R
+++ fixingsfcode1022021.R
@@ -121,3 +121,131 @@ pfruit[,sf2:= sf2/length(nsubj)]
 ggplot(data= data.frame(pfruit), aes(x= sf2, y= sf2sf1, color= "Fruits"))+ geom_count(alpha= 0.3)+ geom_abline() + labs(x= "Sum(SF2)-1/length(N Subjects)", y= "Sum(SF2 & SF1)/ Sum(SF1)", color= "Category", size= "Frequency")+ geom_count(data= data.frame(pveg), aes(x= vf2, y= vf2vf1, color= "Vegetables"), alpha= 0.3)+ geom_count(data= data.frame(panimal), aes(x= af2, y= af2af1, color= "Animals"), alpha= 0.3)
 ggsave("fixed_sfcomparison10032021", device= "png", dpi= 300)
 
+
+################### Something like an SPC? ###################
+
+
+
+s1 <- fruit[, sp:= 0]
+s2 <- vector()
+
+for (i in 1:length(unique(s1[,id]))){
+  s1[id== unique(id)[i] & listrank== 1]$sp <- 1:length(fruit[id== unique(id)[i] & listrank== 1]$id)
+  s1[id== unique(id)[i] & listrank== 2]$sp <- 1:length(fruit[id== unique(id)[i] & listrank== 2]$id)
+  s1[id== unique(id)[i] & listrank== 3]$sp <- 1:length(fruit[id== unique(id)[i] & listrank== 3]$id)
+}
+
+sp1 <- vector()
+for (i in 1:length(unique(s1[listrank== 1,item]))){
+  sp1[i]= sum(s1[listrank>1,item]== unique(s1[listrank== 1,item])[i])/(length(nsubj)*2)
+}
+
+sp3 <- matrix()
+
+
+for (i in 1:length(unique(nsubj))){
+  this_subj <- s1[id== nsubj[i],]
+  for (j in 1:10){
+    sp3[i,j:j+length(j)] <- sum(this_subj[,sp]==j)
+  }
+}
+
+
+ggplot()+ geom_line(aes(x= 1:length(unique(s1[listrank== 1,item])), y= sp1))
+# s1[id== unique(id)[1]]$sp <- 1:length(fruit[id== unique(id)[1]]$id)
+# s1[id== unique(id)[2]]$sp <- 1:length(fruit[id== unique(id)[2]]$id)
+# s1[id== unique(id)[3]]$sp <- 1:length(fruit[id== unique(id)[3]]$id)
+# s1[id== unique(id)[4]]$sp <- 1:length(fruit[id== unique(id)[4]]$id)
+
+(s1 <- s1[sp>0,])
+
+# Get shortest Output length
+(k <- s1[, .N, by= id])
+k[N==min(N)]$id
+sp1 <- s1[item== s1[1, item] & listrank== 1]
+sp2 <- s1[item== s1[1, item] & listrank== 2]
+
+s2[1] <- length(sp2$sp)/length(sp1$sp) 
+s2[2]<- length(s1[item== unique(s1$item)[2] & listrank== 2]$sp)/length(s1[item== unique(s1$item)[2] & listrank== 1]$sp) 
+
+
+s2[3]<- length(s1[item== unique(s1$item)[3] & listrank== 2]$sp)/length(s1[item== unique(s1$item)[3] & listrank== 1]$sp) 
+
+s2[4]<- length(s1[item== unique(s1$item)[4] & listrank== 2]$sp)/length(s1[item== unique(s1$item)[4] & listrank== 1]$sp)
+
+nfruit <- unique(fruit$item)
+
+length(unique(s1[id== unique(id)[1] & item== item[1],]$listrank))/3 #repeated trials
+s1[id== unique(id)[2] & item== item[1],]
+
+sum(s1[,sp]==1)
+
+
+
+
+
+
+
+
+# For loop through item listed in first trial (i)
+ses_1 <- s1[id== id[1] & listrank== 1]$item
+# Find index of item listed in list 2
+ses_2 <- s1[id== id[1] & listrank== 2]$item
+# Find repeated items for subject[i]
+common_items <- intersect(ses_1, ses_2)
+# New for loop through common items within other for loop (j)
+# Assign "sp/rp" from item in first trial to second trial 
+s1[id== unique(id)[1] & item== common_items[1] & listrank == 2]$sp = s1[item== common_items[1] & id== unique(id)[1] & listrank == 1, sp]
+
+# Set the second trial sp to 0 for now
+s1[listrank== 2]$sp<- 0
+for (subj in 1:length(nsubj)){
+    # 'This Subject' Var.  Indexing variable for a specific subject
+  this_subj1 <- s1[id== unique(id)[subj] & listrank == 1]
+  this_subj2 <- s1[id== unique(id)[subj] & listrank == 2]
+  common_items <- intersect(this_subj1$item, this_subj2$item)
+  for (i in 1:length(common_items)){
+    # This is a super annoying work around, but I'm basically going through and finding the repeated items and matching them to the repeated trial.  Then I'm going in and assigning the RP from the first trial to the second trial.  
+    s1[id== unique(this_subj1$id) & listrank== 2 & item== common_items[i]]$sp= first(s1[id== unique(this_subj1$id) & listrank== 1 & item== common_items[i]]$sp)
+  }
+}
+# We don't really care about the third repetition ATM so just take it out. It's getting confusing
+s1 <- s1[listrank<3,]
+########################### REPEATED TRANSITIONS NOT FINISHED ##################################
+# s1[id== "A101"]
+# (full_transitions= matrix(rep(NaN, 200), nrow= 20, ncol= 100))
+# 
+# 
+# transitions <- vector()
+# transitions <- NaN
+# for (subj in 1:length(nsubj)){
+#   transitions <- vector() #clear transitions var
+#   for (i in 1:length(s1[id== nsubj[subj] & listrank== 2,sp])-1){
+#     this_subj= s1[id== nsubj[subj] & listrank== 2,]
+#     if (sum(this_subj$sp[i+1], this_subj$sp[i])==0){
+#       transitions[i]= NaN
+#     }else{
+#       transitions[i]= this_subj$sp[i+1] - this_subj$sp[i]
+#     }
+#   }
+#   full_transitions[subj,1:i] <- transitions
+# }
+# (n_transitions <- as.vector(sort(unique(full_transitions[!is.nan(full_transitions[,])]))))
+# 
+# rownames(full_transitions) <- nsubj
+# 
+# # full_transitions <- data.table(full_transitions)
+# (full_transitions <- full_transitions[,1:30])
+# 
+# nonnan_transitions <- full_transitions[!is.nan(full_transitions)]
+# 
+# sum(nonnan_transitions== n_transitions[26])/length(nonnan_transitions)
+# p_transitions <- vector()
+# for (i in 1:length(n_transitions)){
+#   p_transitions[i] <- sum(nonnan_transitions== n_transitions[i])/length(nonnan_transitions)
+# }
+# 
+# 
+# ggplot()+ geom_line(aes(x= n_transitions, y= p_transitions))+ geom_point(aes(x= n_transitions, y= p_transitions))+ labs(x= "Transition", y= "p(Transition)")
+  
+
