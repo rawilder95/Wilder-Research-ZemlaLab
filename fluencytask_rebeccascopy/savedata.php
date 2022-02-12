01035c4e73a83a5caae5a2b1a403b746e0feb348
Jeff Zemla <jeffzemla@gmail.com>
2022-02-12 21:43:35 +0000

    server write

--- fluencytask_rebeccascopy/savedata.php
+++ fluencytask_rebeccascopy/savedata.php
@@ -3,28 +3,28 @@
     session_start();
 
     // Disable magic quotes
-    if (get_magic_quotes_gpc()) {
-        $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
-        while (list($key, $val) = each($process)) {
-            foreach ($val as $k => $v) {
-                unset($process[$key][$k]);
-                if (is_array($v)) {
-                    $process[$key][stripslashes($k)] = $v;
-                    $process[] = &$process[$key][stripslashes($k)];
-                } else {
-                    $process[$key][stripslashes($k)] = stripslashes($v);
-                }
-            }
-        }
-        unset($process);
-    }
+    //if (get_magic_quotes_gpc()) {
+    //    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    //    while (list($key, $val) = each($process)) {
+    //        foreach ($val as $k => $v) {
+    //            unset($process[$key][$k]);
+    //            if (is_array($v)) {
+    //                $process[$key][stripslashes($k)] = $v;
+    //                $process[] = &$process[$key][stripslashes($k)];
+    //            } else {
+    //                $process[$key][stripslashes($k)] = stripslashes($v);
+    //            }
+    //        }
+    //    }
+    //    unset($process);
+    //}
 
-    $file = "S".$_SESSION['userid'].$_SESSION."_data.txt";
+    $file = "S".$_SESSION['userid']."_data.txt";
     $file = fopen('./logs/'.$file, 'w');
     fwrite($file, $_POST['games_json']);
     fclose($file);    
 
-    $file = "S".$_SESSION['userid'].$_SESSION."_distractor.txt";
+    $file = "S".$_SESSION['userid']."_distractor.txt";
     $file = fopen('./logs/'.$file, 'w');
     fwrite($file, $_POST['distractor_json']);
     fclose($file);    
