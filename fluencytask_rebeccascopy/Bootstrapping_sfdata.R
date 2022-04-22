31b1e6a97370ef1bd65bdc9f8c5210784b3b00a7
REBECCA Anne WILDER <71036276+rawilder95@users.noreply.github.com>
2022-04-22 10:07:30 -0400

    updated analysis scripts

--- fluencytask_rebeccascopy/Bootstrapping_sfdata.R
+++ fluencytask_rebeccascopy/Bootstrapping_sfdata.R
@@ -121,9 +121,9 @@ bts_table= data.table(id= character(), category= character(), oldold= numeric(),
 
 
 
+btransition_probabilities= data.table(id= character(), category= character(), OldOld= numeric(), OldNew= numeric(), NewOld= numeric(), NewNew= numeric(), Old= numeric(), New= numeric())
 
-
-for (bts in 1:1000){
+for (bts in 1:100){
   for (subject in nsubj){
     for (cats in unique(dat$category)){
       bold_old <-0 
@@ -133,6 +133,7 @@ for (bts in 1:1000){
       bold <- 0
       bnew <-0 
       trial2 <- dat[id== subject & category== cats & listrank==2,]
+      trial2$both_trials <- sample(trial2$both_trials)
       for (i in 1:(nrow(trial2)-1)){
         if(trial2[i]$both_trials== 1 & trial2[i+1]$both_trials== 1){
           bold_old= bold_old+1
@@ -145,10 +146,44 @@ for (bts in 1:1000){
         }
       }
       newrow <- list(id=subject, category=cats, OldOld=bold_old, OldNew=bold_new, NewNew=bnew_new, NewOld=bnew_old)
-      btransition_probabilities <- rbind(transition_probabilities, newrow, fill= TRUE)
+      btransition_probabilities <- rbind(btransition_probabilities, newrow, fill= TRUE)
     }
   }
 }
 
+
+# btransition_probabilities <- subset(btransition_probabilities, select = -c(boldold, boldnew, bnewold, bnewnew))
+
+for (bts in 1:100){
+  for (subject in nsubj){
+    for (cats in unique(dat$category)){
+          old <- 0
+          new <- 0
+          trial2 <- dat[id== subject & category== cats & listrank==2,]
+          for (i in 1:nrow(trial2)){
+            if (trial2$both_trials[i]==1){
+              old= old +1
+            } else {
+              new = new + 1
+            }
+          }
+          btransition_probabilities[id== subject & category== cats]$Old= old
+          btransition_probabilities[id== subject & category== cats]$New= new
+        }
+      }
+        }
+      
+      
+bt <- btransition_probabilities
+
+bnew_dat <- data.table()
+bnew_dat[, bt_vals:= c(bt$OldOld/(bt$OldOld+bt$OldNew), bt$OldNew/(bt$OldOld+bt$OldNew), bt$NewOld/(bt$NewOld+bt$NewNew), bt$NewNew/(bt$NewOld+bt$NewNew))]
+
+
+bnew_dat[, bt_labs:= c(rep("p(Old|Old)", length(bt$OldOld)), rep("p(New|Old)", length(bt$OldNew)), rep("p(Old|New)", length(bt$NewOld)), rep("p(New|New)", length(bt$NewNew)))]
+
+
+ggplot() + geom_boxplot(aes(x= bnew_dat$bt_labs, y= bnew_dat$bt_vals))+ labs(x= "Retrieval Type", y= "Conditional Probability of Retrieval Type")
+ggsave("conditional_probability_repeat_type.png", device= "png", dpi= 300)
 
 
